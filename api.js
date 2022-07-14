b4d3d4812e9a7c0757c05f0f6ec44f011859989f
Rahul Chaurasiya <rahulch295@gmail.com>
2022-07-14 15:41:01 +0530

    Update API URL

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ exports.getPickupLines = async () => {
   var data = "";
   var config = {
     method: "get",
-    url: "https://cheempickupsapi.herokuapp.com/joke",
+    url: "https://cheemspickupapi.herokuapp.com/joke",
     headers: {},
     data: data,
   };
